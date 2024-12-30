1ac993649d160de93a170a6b9ae05c46c79187f6
Slytech Root <96862479+slytechroot@users.noreply.github.com>
2024-12-30 14:09:30 -0800

    Update 1.2_read_integer_and_print_corresponding_multiplication_table.c

--- edx-master/edx-master/1.2_read_integer_and_print_corresponding_multiplication_table.c
+++ edx-master/edx-master/1.2_read_integer_and_print_corresponding_multiplication_table.c
@@ -15,3 +15,20 @@ int main(void)
 }
 
 /*In this activity, you want to improve your existing multiplication program (that prints the 8 times table). Your program should read an integer from the user (not you) and print the multiplication table for the number that they enter.*/
+
+# OR 
+
+#include <stdio.h>
+
+int main() {
+
+    int i, num;
+
+    scanf("%d", &num);
+
+    for (i = 0; i <= 10; i++){
+        printf("%dx%d = %d\n", i, num, i*num);
+    }
+
+    return(0);
+}
