1e14c681b78cebeb1fabe89a8e76381c3e419ae0
Slytech Root <96862479+slytechroot@users.noreply.github.com>
2024-12-30 16:40:45 -0800

    Update 1.3_read_characters_from_user_input_and_produce_ASCII_art.c

--- edx-master/edx-master/1.3_read_characters_from_user_input_and_produce_ASCII_art.c
+++ edx-master/edx-master/1.3_read_characters_from_user_input_and_produce_ASCII_art.c
@@ -24,3 +24,21 @@ Output
 +#######+
 #########
 */
+
+Class solution:
+#include <stdio.h>
+
+int main(void) {
+
+    char l;
+
+    scanf("%c", &l);
+
+    printf("++++%c++++\n", l);
+    printf("+++%c%c%c+++\n", l, l, l);
+    printf("++%c%c%c%c%c++\n", l, l, l, l, l);
+    printf("+%c%c%c%c%c%c%c+\n", l, l, l, l, l, l, l);
+    printf("%c%c%c%c%c%c%c%c%c\n", l, l, l, l, l, l, l, l, l);
+
+    return(0);
+}
