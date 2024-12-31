d62b8fe9a98c774e6b226e29d98f84c1f739a8fb
Slytech Root <96862479+slytechroot@users.noreply.github.com>
2024-12-30 16:48:25 -0800

    Create 1.3_Divide in C.c

---
+++ edx-master/edx-master/1.3_Divide in C.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    // integer division
+    printf("5/2 equals %d\n", 5/2);
+    // floating point division
+    printf("5.0/2.0 equals %lf\n", 5.0/2.0);
+    printf("5/2.0 equals %lf\n", 5/2.0);
+    printf("5.0/2 equals %lf\n", 5.0/2);
+    return 0;
+}
