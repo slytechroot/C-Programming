fc5fec20e6c01e1e40e936e4c8ec7c12fc8c4b77
Slytech Root <96862479+slytechroot@users.noreply.github.com>
2025-01-07 15:49:05 -0800

    Update 3.1_Differentiate-the-prototype-and-the-definition-of-a-function.c

--- edx-master/edx-master/3.1_Differentiate-the-prototype-and-the-definition-of-a-function.c
+++ edx-master/edx-master/3.1_Differentiate-the-prototype-and-the-definition-of-a-function.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-int sum(int, int); //function prototype
-
+int sum(int, int); //function PROTOTYPE
 int main(void) {
     int a, b;
     int result;
@@ -12,6 +10,7 @@ int main(void) {
     printf("Result of the sum = %d.\n", result);
     return 0;
 }
+// Function DEFINITION
 int sum(int x, int y){ //values are copied into PARAMETERS x and y
     int compute;
     printf("Starting the computation!\n");
