19f47c4fae76883b159f5a80e05406060537e83c
Sly <96862479+slytechroot@users.noreply.github.com>
2025-01-30 14:10:16 -0800

    Create 5.1.4_Activity-function-to-find-smallest-integer.c

---
+++ edx-master/edx-master/5.1.4_Activity-function-to-find-smallest-integer.c
@@ -0,0 +1,62 @@
+The goal of this problem is to find the smallest integer in a list of numbers.
+
+To help you with this task, please write a function called min() that finds and returns the smallest amongst two integers (be sure to also write a prototype for this function). The function thus takes two integers as input and returns the smallest of the two. This function will use an if statement with a condition that contains either "less than" or "greater than".
+
+Next, please use min() in your main function to work your way through an entire list of numbers in order to find its minimum. The first number you read gives the number of elements in the list of integers under consideration. You will then read the integer values, using min() to keep only the smallest integer read at each step. In the end, please print out the smallest integer in the list.
+Example
+Input
+
+10
+4 3 6 2 6 8 9 8 5 4
+
+Output
+
+2
+
+Warning: You will be graded on your output, so do not include any print statements that prompt a user for input.
+  
+###########################
+
+#include <stdio.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    int n, num, smallest;
+    scanf("%d", &n);
+    
+    if (n > 0) {
+        scanf("%d", &smallest);
+        for (int i = 1; i < n; i++) {
+            scanf("%d", &num);
+            smallest = min(smallest, num);
+        }
+        printf("%d\n", smallest);
+    }
+    
+    return 0;
+}
+
+####################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
