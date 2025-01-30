1b7530d20b74424aa7967146727f159532019589
Sly <96862479+slytechroot@users.noreply.github.com>
2025-01-30 10:10:24 -0800

    Create 3.1.5_Correct_basic_compilation_errors_with_functions.c

---
+++ edx-master/edx-master/3.1.5_Correct_basic_compilation_errors_with_functions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//prototype
+int sum(int, int);
+
+int main(void){
+  int sum;
+  result = sum(3,4);
+  printf("The sum is %d.\n", result);
+  return 0;
+}
+
+//the sum function
+int sum(int x, int y){
+  int compute;
+  compute = x*y;
+  return(compute);
+}
